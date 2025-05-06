63c5e4b49293e815d516efe1c3c05540e699211d
Akhil@151199 <ar.akhil1511@gmail.com>
2025-05-06 11:57:52 +0530

    Fix : Added Basename so github pages doesnt throw 404.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import ExchangeRates from "../pages/ExchangeRates";
@@ -6,7 +6,7 @@ import ErrorPage from "../pages/ErrorPage";
 
 export default function AppRoutes() {
   return (
-    <Router basename="/LoanCalculator">
+    <Router>
       <Routes>
         <Route element={<MainLayout />}>
           <Route index element={<Home />} />
